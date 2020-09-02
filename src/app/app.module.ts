1d55b1d94838442ec1c4be011a91626d42bb97fc
unknown <marnix_decloedt@hotmail.com>
2020-09-02 21:33:11 +0200

    pwa 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NewsapiService } from './services/newsapi.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ArticlesTechnologyComponent } from './articles-technology/articles-technology.component';
+import { MatCardModule } from '@angular/material';
+import { ArticlesJavascriptComponent } from './articles-javascript/articles-javascript.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ArticlesTechnologyComponent,
+    ArticlesJavascriptComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [NewsapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/newsapi.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsapiService {
+
+  api_key = 'e51f2e30fe794bb5abb2f5b1489dedae';
+
+  constructor(private http: HttpClient) { }
+
+  getArticlesTechnology(): Observable<any> {
+    return this.http
+      .get(
+        'https://newsapi.org/v2/top-headlines?category=technology&language=en&country=us&apiKey=' +
+          this.api_key
+      )
+      .pipe(map((data: any) => data.articles));
+  }
+
+  getArticlesJavaScript(): Observable<any> {
+    return this.http
+      .get(
+        'https://newsapi.org/v2/everything?q=javascript&sortBy=latest&apiKey=' +
+          this.api_key
+      )
+      .pipe(map((data: any) => data.articles));
+  }
+}
